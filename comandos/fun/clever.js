61986cc0785248e87d819079528a9f049b97bab7
dreaaaaaamer <43127039+dreaamer@users.noreply.github.com>
2018-09-12 09:22:24 -0300

    😋

--- comandos/fun/clever.js
+++ comandos/fun/clever.js
@@ -3,7 +3,7 @@ const cleverbot = require("cleverbot.io"),
 
 exports.run = (bot, message, args, Discord, disEmbed) => {
   let msg = args.join(" ");
-    if(!msg) return message.reply("<:error:485264317734846464> - Use kf.clever plus your argument.", "<:error:485264317734846464> - Type a message to send");
+    if(!msg) return message.reply("<:error:485264317734846464> - Use **kf.clever** plus your argument.", "<:error:485264317734846464> - Type a message to send");
     message.channel.startTyping();
       cbot.setNick("kofuku");
 	  	cbot.create(function(err, session) {
