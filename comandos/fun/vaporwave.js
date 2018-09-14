0e939b9056c9201714e500b06c9ed65311ab8c40
dreaaaaaamer <43127039+dreaamer@users.noreply.github.com>
2018-09-14 02:48:21 -0300

    😋

--- comandos/fun/vaporwave.js
+++ comandos/fun/vaporwave.js
@@ -1,6 +1,10 @@
-
+const cooldown = new Set()
 
 module.exports.run = async (bot, message, args) => {
+
+	  if (cooldown.has(message.author.id)) {
+    message.channel.send('<:gloock:488829272664965130> - Wait **30** seconds to use this command again.')
+  } else {
 console.log(`[Lala LOGS CMD] Usaram o comando "vaporwave" - Nome: ${message.author.username} (${message.author.id}) Server: ${message.guild.name} (${message.guild.id}`)
 
 if(!args[0]) return message.reply("<:error:485264317734846464> - You need to put something to me to talk.");
@@ -16,6 +20,13 @@ String.prototype.toFullWidth = function() {
 
 message.channel.send(msgstringVapor.toFullWidth());
 }
+
+    cooldown.add(message.author.id)
+    setTimeout(() => {
+      cooldown.delete(message.author.id)
+    }, 30000)
+  }
+
 
 module.exports.help = {
     name: "vaporwave"
