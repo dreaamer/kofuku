87f0457fe37b47cc05acb00897a84370afeab21d
dreaaaaaamer <43127039+dreaamer@users.noreply.github.com>
2018-09-17 17:46:24 -0300

    😋

--- comandos/info/info.js
+++ comandos/info/info.js
@@ -5,7 +5,7 @@ const cooldown = new Set()
 
 exports.run = (client, message, args, guild) => {
         if (cooldown.has(message.author.id)) {
-    message.channel.send('<:gloock:488829272664965130> - Wait **30** seconds to use this command again.')
+    message.channel.send('<:gloock:488829272664965130> - Wait **5** seconds to use this command again.')
   } else {
 
     var dreamer = client.guilds.get("321077526568894465").members.get("321076625502371852").user.tag
@@ -96,7 +96,7 @@ exports.run = (client, message, args, guild) => {
     cooldown.add(message.author.id)
     setTimeout(() => {
       cooldown.delete(message.author.id)
-    }, 30000)
+    }, 5000)
   }
 
 
