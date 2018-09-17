2668eb8f706f3b0e2770db5528ad642128e5e8b6
dreaaaaaamer <43127039+dreaamer@users.noreply.github.com>
2018-09-17 17:44:30 -0300

    😋

--- comandos/image/discord.js
+++ comandos/image/discord.js
@@ -5,7 +5,7 @@ const cooldown = new Set()
 
 exports.run = (client, message, args) => {
           if (cooldown.has(message.author.id)) {
-    message.channel.send('<:gloock:488829272664965130> - Wait **30** seconds to use this command again.')
+    message.channel.send('<:gloock:488829272664965130> - Wait **5** seconds to use this command again.')
   } else {
     console.log(`[Lala LOGS CMD] Usaram o comando "hex" - Nome: ${message.author.username} (${message.author.id}) Server: ${message.guild.name} (${message.guild.id}`)
 
@@ -31,6 +31,6 @@ exports.run = (client, message, args) => {
                                 cooldown.add(message.author.id)
     setTimeout(() => {
       cooldown.delete(message.author.id)
-    }, 30000)
+    }, 5000)
   }
                 
