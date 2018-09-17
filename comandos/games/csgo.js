3845761c38c262a9528849d31f5b6594678f3cc3
dreaaaaaamer <43127039+dreaamer@users.noreply.github.com>
2018-09-17 17:42:52 -0300

    😋

--- comandos/games/csgo.js
+++ comandos/games/csgo.js
@@ -6,7 +6,7 @@ const cooldown = new Set()
 exports.run = (client, message, args) => {
 
       if (cooldown.has(message.author.id)) {
-    message.channel.send('<:gloock:488829272664965130> - Wait **30** seconds to use this command again.')
+    message.channel.send('<:gloock:488829272664965130> - Wait **5** seconds to use this command again.')
   } else {
   console.log(`[Lala LOGS CMD] Usaram o comando "csgo" - Nome: ${message.author.username} (${message.author.id}) Server: ${message.guild.name} (${message.guild.id}`)
   let steamID = args[0];
@@ -65,7 +65,7 @@ exports.run = (client, message, args) => {
     cooldown.add(message.author.id)
     setTimeout(() => {
       cooldown.delete(message.author.id)
-    }, 30000)
+    }, 5000)
   }
 
 
