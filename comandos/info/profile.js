a0c06b5ffee110860e22664f4441d95548bfcf71
dreaaaaaamer <43127039+dreaamer@users.noreply.github.com>
2018-09-17 17:47:29 -0300

    😋

--- comandos/info/profile.js
+++ comandos/info/profile.js
@@ -3,7 +3,7 @@ const cooldown = new Set()
 
 exports.run = (client, message, args) => {
         if (cooldown.has(message.author.id)) {
-    message.channel.send('<:gloock:488829272664965130> - Wait **30** seconds to use this command again.')
+    message.channel.send('<:gloock:488829272664965130> - Wait **5** seconds to use this command again.')
   } else {
 console.log(`[Lala LOGS CMD] Usaram o comando "profile" - Nome: ${message.author.username} (${message.author.id}) Server: ${message.guild.name} (${message.guild.id}`)
   let user = message.mentions.users.first();
@@ -134,5 +134,5 @@ console.log(`[Lala LOGS CMD] Usaram o comando "profile" - Nome: ${message.author
     cooldown.add(message.author.id)
     setTimeout(() => {
       cooldown.delete(message.author.id)
-    }, 30000)
+    }, 5000)
   }
